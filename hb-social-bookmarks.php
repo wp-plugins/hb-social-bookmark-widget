ae0c3a26080f10d14b13bb45ba57831fa477e70a
HendrikBerlin <HendrikBerlin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-14 18:40:32 +0000

    fixed path name to Icons
    git-svn-id: https://plugins.svn.wordpress.org/hb-social-bookmark-widget/trunk@40663 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hb-social-bookmarks.php
+++ hb-social-bookmarks.php
@@ -82,116 +82,116 @@ class hb_social_bookmark {
 				<a href="javascript:AddFavorite();"
 					onmouseover="displayBookmark(\'Favoriten\')" onmouseout="displayBookmark(\'\')"	
 					title="Zu Browser Favoriten hinzuf&uuml;gen">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_favorites.png" alt="Browser Favoriten" /></a>
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_favorites.png" alt="Browser Favoriten" /></a>
 					
 				<!-- Mister Wong -->
 				<a href="http://www.mister-wong.de/?action=addurl&bm_url='.$url.'&bm_description='.$title.'"
 					onmouseover="displayBookmark(\'Mr. Wong\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Mr. Wong bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_wong_16.gif" alt="Mr. Wong" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_wong_16.gif" alt="Mr. Wong" /></a>
 				
 				<!-- Facebook -->
 				<a href="http://www.facebook.com/share.php" 
 					onmouseover="displayBookmark(\'Facebook\')" onmouseout="displayBookmark(\'\')"
 					onclick="return fbs_click()" title="Bei Facebook merken"
 					target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_facebook.gif" alt="Facebook" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_facebook.gif" alt="Facebook" /></a>
 
 				<!-- StumbleUpon -->
 				<a href="http://www.stumbleupon.com/submit?url='.$url.'"
 					onmouseover="displayBookmark(\'StumbleUpon\')" onmouseout="displayBookmark(\'\')" 
 					title="Bei StumbledUpon bookmarken">
-					<img border=0 src="/wp-content/plugins/hb-social-bookmarks/book_su.gif" alt="StumbleUpon">
+					<img border=0 src="/wp-content/plugins/hb-social-bookmark-plugin/book_su.gif" alt="StumbleUpon">
 					</a>
 					
 				<!-- Technorati -->
 				<a href="http://www.technorati.com/faves/?add='.$url.'"
 					onmouseover="displayBookmark(\'Technorati\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Technorati bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_technorati.png"
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_technorati.png"
 					alt="technorati" /></a>
 		
 				<!-- Furl -->
 				<a href="http://furl.net/storeIt.jsp?u='.$url.'&t='.$title.'"
 					onmouseover="displayBookmark(\'Furl\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Furl speichern" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_furl.png" alt="Furl" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_furl.png" alt="Furl" /></a>
 				
 				<!-- Del.icio.us -->
 				<a href="http://del.icio.us/post?url='.$url.'&title='.$title.'"
 					onmouseover="displayBookmark(\'Del.icio.us\')" onmouseout="displayBookmark(\'\')"
 					title="Bei del.icio.us bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_delicious.png"
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_delicious.png"
 					alt="del.icio.us" /></a>
 				
 				<!-- Yahoo -->
 				<a href="http://myweb2.search.yahoo.com/myresults/bookmarklet?u='.$url.'&t='.$title.'"
 					onmouseover="displayBookmark(\'Yahoo\')" onmouseout="displayBookmark(\'\')"
 					title="Zu Mein Yahoo hinzufügen" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_yahoo.png" alt="yahoo" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_yahoo.png" alt="yahoo" /></a>
 				
 				<!-- Linkarena -->
 				<a href="http://www.linkarena.com/bookmarks/addlink/?url='.$url.'&title='.$title.'"
 					onmouseover="displayBookmark(\'Linkarena\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Linkarena bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_linkarena.png" alt="Linkarena" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_linkarena.png" alt="Linkarena" /></a>
 				
 				<!-- Oneview -->
 				<a href="http://oneview.de/quickadd/neu/addBookmark.jsf?URL='.$url.'&title='.$title.'"
 					onmouseover="displayBookmark(\'Oneview\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Oneview bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_oneview.png" alt="Oneview" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_oneview.png" alt="Oneview" /></a>
 				
 				<!-- Webnews -->
 				<a href="http://www.webnews.de"
 					onmouseover="displayBookmark(\'Webnews\')" onmouseout="displayBookmark(\'\')" 
 					onclick="document.location = \'http://www.webnews.de/einstellen?url=\'+encodeURIComponent(document.location)+\'&title=\'+encodeURIComponent(document.title); return false;" 
 					title="Diesen Beitrag bei Webnews verlinken">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_webnews.gif" alt="Webnews" /></a>
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_webnews.gif" alt="Webnews" /></a>
 				
 				<!-- Digg -->
 				<a href="http://digg.com/submit?url='.$url.'&title='.$title.'&bodytext='.$description.'&media=news&topic=tech_news"
 					onmouseover="displayBookmark(\'Digg\')" onmouseout="displayBookmark(\'\')" 
 					title="Digg it">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_digg-16x16.png" width="16" height="16" alt="Digg!" />
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_digg-16x16.png" width="16" height="16" alt="Digg!" />
 					</a>
 					
 				<!-- Myspace -->
 				<a href="javascript: GetThis(\''.$title_iso.' \', \''.$description_iso.' \', \' '.$url.' \', 5)"
 					onmouseover="displayBookmark(\'MySpace\')" onmouseout="displayBookmark(\'\')" 
 					title="Bei MySpace speichern">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_myspace.gif" alt="MySpace" /></a>
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_myspace.gif" alt="MySpace" /></a>
 					
 				<!-- Windows live -->
 				<a href="https://favorites.live.com/quickadd.aspx?marklet=1&mkt=de&url='.$url.'&title='.$title.'&top=1"
 					onmouseover="displayBookmark(\'Windows Live\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Windows live Favorites bookmarken">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_live.png" alt="Windows Live" /></a>
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_live.png" alt="Windows Live" /></a>
 
 				<!-- Google -->
 				<a href="http://www.google.com/bookmarks/mark?op=add&hl=de&bkmk='.$url.'&title='.$title.'"
 					onmouseover="displayBookmark(\'Google\')" onmouseout="displayBookmark(\'\')"
 					title="Bei iGoogle speichern" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_google.png" alt="google" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_google.png" alt="google" /></a>
 					
 				<!-- folkd -->
 				<a href="http://www.folkd.com/submit/'.$url.'"
 					onmouseover="displayBookmark(\'folkd\')" onmouseout="displayBookmark(\'\')"
 					title="Bei folkd bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_folkd.png" alt="folkd" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_folkd.png" alt="folkd" /></a>
 
 				<!-- Blinklist -->
 				<a href="http://www.blinklist.com/?Action=Blink/addblink.php&Url='.$url.'&Title='.$title.'&Tag=&Description='.$description.'"
 					onmouseover="displayBookmark(\'Blinklist\')" onmouseout="displayBookmark(\'\')"
 					title="Bei Blinklist bookmarken" target="_blank"><img
-					src="/wp-content/plugins/hb-social-bookmarks/book_blinklist.png" alt="Blinklist" /></a>
+					src="/wp-content/plugins/hb-social-bookmark-plugin/book_blinklist.png" alt="Blinklist" /></a>
 					
 				<!-- Favoriten.de -->
 					<a href="http://www.favoriten.de/url-hinzufuegen.html" 
 					onmouseover="displayBookmark(\'Favoriten.de\')" onmouseout="displayBookmark(\'\')"
 					onClick="location.href=&quot;http://www.favoriten.de/url-hinzufuegen.html?bm_url=&quot;+encodeURIComponent(location.href)+&quot;&amp;bm_title=&quot;+encodeURIComponent(document.title);return false" 
 					title="Bei Favoriten.de bookmarken" target="_top">
-					<img src="/wp-content/plugins/hb-social-bookmarks/book_favoritende.gif" alt="Favoriten.de" border="0" /></a>
+					<img src="/wp-content/plugins/hb-social-bookmark-plugin/book_favoritende.gif" alt="Favoriten.de" border="0" /></a>
 			</li>
 		';
 			
