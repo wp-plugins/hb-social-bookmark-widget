406afad1336a26ab337da64d0bb6231cf04b482d
HendrikBerlin <HendrikBerlin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-14 09:51:16 +0000

    initial check in of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/hb-social-bookmark-widget/trunk@40572 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hb-social-bookmarks.php
@@ -0,0 +1,211 @@
+<?php
+/*
+Plugin Name: HB Social Bookmarks
+Plugin URI: http://www.hendrikbahr.de/
+Description: Plugin displays several Social Bookmark Icons in the sidebar as a widget.
+Author: Hendrik Bahr
+Version: 1.0
+Author URI: http://www.hendrikbahr.de/
+*/
+class hb_social_bookmark {
+	function init(){
+		$widget = new hb_social_bookmark();
+    	register_sidebar_widget('HB Social Bookmarks', array($widget,'display'));
+	}
+	/* Function Display
+	 * echos HTML Output for Social bookmark widget
+	 */
+	function display(){
+		/*Preparing variables */
+			$url 				= 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+		
+		if (is_single() || is_page()) {
+			$description_iso 	= get_the_excerpt();
+			$description 		= urlencode($description_iso);
+			$title_iso			= get_the_title();
+			$title				= urlencode($title_iso);
+		}elseif (is_home()) {
+			$description_iso 	= get_bloginfo('description');
+			$description 		= urlencode($description_iso);
+			$title_iso 			= get_bloginfo('name');
+			$title 				= urlencode($title_iso);
+		}elseif (is_archive()){
+			$description_iso 	= single_tag_title('', false).' - '.get_bloginfo('description');
+			$description 		= urlencode($description_iso);		
+			$title_iso 			= single_tag_title('', false);
+			$title 				= urlencode($title_iso);	
+		}else {
+			$description_iso 	= '';
+			$description 		= '';
+			$title_iso 			= '';
+			$title 				= '';
+		}
+
+		/* printing Output */
+		echo '
+		    <script type="text/javascript">
+			<!--
+				/* Function for Displaying Platform Title */
+				function displayBookmark(text){
+					myDiv = document.getElementById(\'bookmark\');
+					myDiv.innerHTML = text;
+			    	}
+				/* Function for Add to Facebook */
+				function fbs_click() {
+				        u=location.href;
+				        t=document.title;
+				        window.open("http://www.facebook.com/sharer.php?u="+encodeURIComponent(u)+"&t="+encodeURIComponent(t),"sharer","toolbar=0 ,status=0,width=626,height=436");
+				        return false;
+			        }
+			    /* Function for Add to MySpace */
+			    function GetThis(T, C, U, L){
+						var targetUrl = "http://www.myspace.com/Modules/PostTo/Pages/?"+"t="+encodeURIComponent(T)+"&c="+encodeURIComponent(C)+"&u="+encodeURIComponent(U)+"&l="+L;
+						window.open(targetUrl);
+					}
+				/* Function for adding to browser favorites */
+				function AddFavorite(){
+						if(navigator.appName != "Microsoft Internet Explorer"){
+							window.external.addPanel(\''.$title_iso.'\',\''.$url.'\', \'\')
+						} else {
+							window.external.addFavorite(location.href, \''.$title_iso.'\');
+						}							
+					}
+			-->
+			</script>
+			
+			<li class="widget widget_links">
+				<h2 class="widgettitle">Social Bookmarks</h2>				
+				Bookmarken bei: <span id="bookmark" style="font-weight: bold;"></span>
+				<br /><br />
+				
+				<!-- Favoriten -->
+				<a href="javascript:AddFavorite();"
+					onmouseover="displayBookmark(\'Favoriten\')" onmouseout="displayBookmark(\'\')"	
+					title="Zu Browser Favoriten hinzuf&uuml;gen">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_favorites.png" alt="Browser Favoriten" /></a>
+					
+				<!-- Mister Wong -->
+				<a href="http://www.mister-wong.de/?action=addurl&bm_url='.$url.'&bm_description='.$title.'"
+					onmouseover="displayBookmark(\'Mr. Wong\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Mr. Wong bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_wong_16.gif" alt="Mr. Wong" /></a>
+				
+				<!-- Facebook -->
+				<a href="http://www.facebook.com/share.php" 
+					onmouseover="displayBookmark(\'Facebook\')" onmouseout="displayBookmark(\'\')"
+					onclick="return fbs_click()" title="Bei Facebook merken"
+					target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_facebook.gif" alt="Facebook" /></a>
+
+				<!-- StumbleUpon -->
+				<a href="http://www.stumbleupon.com/submit?url='.$url.'"
+					onmouseover="displayBookmark(\'StumbleUpon\')" onmouseout="displayBookmark(\'\')" 
+					title="Bei StumbledUpon bookmarken">
+					<img border=0 src="/wp-content/plugins/hb-social-bookmarks/book_su.gif" alt="StumbleUpon">
+					</a>
+					
+				<!-- Technorati -->
+				<a href="http://www.technorati.com/faves/?add='.$url.'"
+					onmouseover="displayBookmark(\'Technorati\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Technorati bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_technorati.png"
+					alt="technorati" /></a>
+		
+				<!-- Furl -->
+				<a href="http://furl.net/storeIt.jsp?u='.$url.'&t='.$title.'"
+					onmouseover="displayBookmark(\'Furl\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Furl speichern" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_furl.png" alt="Furl" /></a>
+				
+				<!-- Del.icio.us -->
+				<a href="http://del.icio.us/post?url='.$url.'&title='.$title.'"
+					onmouseover="displayBookmark(\'Del.icio.us\')" onmouseout="displayBookmark(\'\')"
+					title="Bei del.icio.us bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_delicious.png"
+					alt="del.icio.us" /></a>
+				
+				<!-- Yahoo -->
+				<a href="http://myweb2.search.yahoo.com/myresults/bookmarklet?u='.$url.'&t='.$title.'"
+					onmouseover="displayBookmark(\'Yahoo\')" onmouseout="displayBookmark(\'\')"
+					title="Zu Mein Yahoo hinzufügen" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_yahoo.png" alt="yahoo" /></a>
+				
+				<!-- Linkarena -->
+				<a href="http://www.linkarena.com/bookmarks/addlink/?url='.$url.'&title='.$title.'"
+					onmouseover="displayBookmark(\'Linkarena\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Linkarena bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_linkarena.png" alt="Linkarena" /></a>
+				
+				<!-- Oneview -->
+				<a href="http://oneview.de/quickadd/neu/addBookmark.jsf?URL='.$url.'&title='.$title.'"
+					onmouseover="displayBookmark(\'Oneview\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Oneview bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_oneview.png" alt="Oneview" /></a>
+				
+				<!-- Webnews -->
+				<a href="http://www.webnews.de"
+					onmouseover="displayBookmark(\'Webnews\')" onmouseout="displayBookmark(\'\')" 
+					onclick="document.location = \'http://www.webnews.de/einstellen?url=\'+encodeURIComponent(document.location)+\'&title=\'+encodeURIComponent(document.title); return false;" 
+					title="Diesen Beitrag bei Webnews verlinken">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_webnews.gif" alt="Webnews" /></a>
+				
+				<!-- Digg -->
+				<a href="http://digg.com/submit?url='.$url.'&title='.$title.'&bodytext='.$description.'&media=news&topic=tech_news"
+					onmouseover="displayBookmark(\'Digg\')" onmouseout="displayBookmark(\'\')" 
+					title="Digg it">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_digg-16x16.png" width="16" height="16" alt="Digg!" />
+					</a>
+					
+				<!-- Myspace -->
+				<a href="javascript: GetThis(\''.$title_iso.' \', \''.$description_iso.' \', \' '.$url.' \', 5)"
+					onmouseover="displayBookmark(\'MySpace\')" onmouseout="displayBookmark(\'\')" 
+					title="Bei MySpace speichern">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_myspace.gif" alt="MySpace" /></a>
+					
+				<!-- Windows live -->
+				<a href="https://favorites.live.com/quickadd.aspx?marklet=1&mkt=de&url='.$url.'&title='.$title.'&top=1"
+					onmouseover="displayBookmark(\'Windows Live\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Windows live Favorites bookmarken">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_live.png" alt="Windows Live" /></a>
+
+				<!-- Google -->
+				<a href="http://www.google.com/bookmarks/mark?op=add&hl=de&bkmk='.$url.'&title='.$title.'"
+					onmouseover="displayBookmark(\'Google\')" onmouseout="displayBookmark(\'\')"
+					title="Bei iGoogle speichern" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_google.png" alt="google" /></a>
+					
+				<!-- folkd -->
+				<a href="http://www.folkd.com/submit/'.$url.'"
+					onmouseover="displayBookmark(\'folkd\')" onmouseout="displayBookmark(\'\')"
+					title="Bei folkd bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_folkd.png" alt="folkd" /></a>
+
+				<!-- Blinklist -->
+				<a href="http://www.blinklist.com/?Action=Blink/addblink.php&Url='.$url.'&Title='.$title.'&Tag=&Description='.$description.'"
+					onmouseover="displayBookmark(\'Blinklist\')" onmouseout="displayBookmark(\'\')"
+					title="Bei Blinklist bookmarken" target="_blank"><img
+					src="/wp-content/plugins/hb-social-bookmarks/book_blinklist.png" alt="Blinklist" /></a>
+					
+				<!-- Favoriten.de -->
+					<a href="http://www.favoriten.de/url-hinzufuegen.html" 
+					onmouseover="displayBookmark(\'Favoriten.de\')" onmouseout="displayBookmark(\'\')"
+					onClick="location.href=&quot;http://www.favoriten.de/url-hinzufuegen.html?bm_url=&quot;+encodeURIComponent(location.href)+&quot;&amp;bm_title=&quot;+encodeURIComponent(document.title);return false" 
+					title="Bei Favoriten.de bookmarken" target="_top">
+					<img src="/wp-content/plugins/hb-social-bookmarks/book_favoritende.gif" alt="Favoriten.de" border="0" /></a>
+			</li>
+		';
+			
+	}
+	
+	/* Function display Footer
+	 * echos plugin credentials in blog's footer - thank you for supporting the author by not removing it
+	 */
+	function display_footer()
+	{
+		echo 'Social Bookmark Plugin by <a href="http://www.hendrikbahr.de">HendrikBerlin</a>.';
+	}
+	
+}
+add_action('widgets_init',array('hb_social_bookmark','init'));
+//add_action('wp_footer',array('hb_social_bookmark','display_footer'));
+?>
---
+++ readme.txt
@@ -0,0 +1,58 @@
+=== Plugin Name ===
+Contributors: HendrikBerlin
+Donate link: http://www.hendrikbahr.de
+Tags: widget, plugin, sidebar, social, bookmarks
+Requires at least: 2.3.3
+Tested up to: 2.3.3
+Stable tag: trunk
+
+The module is extending Wordpress with a bunch of the most common social bookmark icons as a sidebar widget.
+
+== Description ==
+
+The Social Bookmark plugin is a "ready to run" addon for the Wordpress blogsoftware. It adds a list of 16x16px icons of the most common social bookmark services to your wordpress blog. 
+It comes as a sidebar widget that can be activated and positioned in the admin's widget configuration interface. The plugin takes care of the correct transfer of bookmarking titles, URLs and descriptions individually generated from your content.
+
+
+Included is a mousover-javascript that shows the platform's name in text above the icons.
+
+Icons of the following services are included:
+ * Add to Browser Favorites
+ * Mr. Wong
+ * Facebook
+ * StumbledUpon
+ * Technorati
+ * Furl
+ * Del.ico.us
+ * Yahoo
+ * Linkarena
+ * Oneview
+ * Webnews
+ * Digg
+ * MySpace
+ * Windows Live Favorites
+ * iGoogle
+ * folkd
+ * Blinklist
+ * Favoriten.de
+ 
+Development has began in February 2008 by Hendrik Bahr, Berlin/ Germany. 
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working:
+
+1. Upload the folder hb-social-bookmarks to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress Admin
+3. Place the social bookmark widget on any space in your sidebar through the 'Widgets' menu in WordPress Admin. Use drag and drop. 
+
+== Frequently Asked Questions ==
+
+= What if my favorite social bookmark service is not included? =
+
+Let me know and I will try to add your prefered service to the plugin!
+
+== Screenshots ==
+
+1. Screenshot of the plugin working in authors blog. Styling of the Widget titles etc. depends on your stylesheets!
